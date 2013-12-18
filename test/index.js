3794089f1a53690f10ae4838099cbd626eb301f9
elangelo <samuel.verstraete@gmail.com>
2013-12-18 20:15:27 +0100

    start converting to binary-reader (deferred-queue)

---
+++ test/index.js
@@ -0,0 +1,10 @@
+﻿var exif = require('..').ExifImage;
+
+new exif( {image: './testfile/sanyo-vpcg250.jpg'}, function (error, image) {
+    if (error) { 
+        console.error('error occurred:' + error);
+        }
+    else {
+        console.log('no errors: ' + image);
+        }
+    });
