b6408a96b6fa038797bd74ce220971f5cf1ff989
elangelo <samuel.verstraete@gmail.com>
2013-12-20 13:55:16 +0100

    add testfile

--- test/index.js
+++ test/index.js
@@ -1,41 +1,61 @@
 ﻿var exif = require('..').ExifImage;
 
-//new exif( {image: './testfile/sanyo-vpcg250.jpg'}, function (error, image) {
-//    if (error) { 
-//        console.error('error occurred:' + error);
-//        }
-//    else {
-//        console.log('no errors: ' + image);
-//        }
-//    });
+new exif( {image: './testfile/sanyo-vpcg250.jpg'}, function (error, image) {
+    if (error) 
+    { 
+        console.error('error occurred: ' + error);
+    }
+    else 
+    {
+        if (image)
+        {
+            console.log('---=== ifd0 ===---');
+            console.log(image.image);
+
+            console.log('---=== ifd1 ===---');
+            console.log(image.thumbnailinfo);
+
+            console.log('---=== exif ifd ===---');
+            console.log(image.exif);
+
+            console.log('---=== gps ifd ===---');
+            console.log(image.gps);
+
+            console.log('---=== interoperability ===---');
+            console.log(image.interoperability);
+           
+            console.log('---=== makernotes ===---');
+            console.log(image.makernote);
+        }
+    }
+});
 
 new exif( {image: './testfile/fujifilm-finepix40i.jpg'}, function (error, image) {
     if (error) 
-        { 
-            console.error('error occurred: ' + error);
-        }
+    { 
+        console.error('error occurred: ' + error);
+    }
     else 
+    {
+        if (image)
         {
-            console.log('no errors: ');
-            if (image)
-                {
-                    console.log('---=== ifd0 ===---');
-                    console.log(image.image);
+            console.log('---=== ifd0 ===---');
+            console.log(image.image);
 
-                    console.log('---=== ifd1 ===---');
-                    console.log(image.thumbnailinfo);
+            console.log('---=== ifd1 ===---');
+            console.log(image.thumbnailinfo);
 
-                    console.log('---=== exif ifd ===---');
-                    console.log(image.exif);
+            console.log('---=== exif ifd ===---');
+            console.log(image.exif);
 
-                    console.log('---=== gps ifd ===---');
-                    console.log(image.gps);
+            console.log('---=== gps ifd ===---');
+            console.log(image.gps);
 
-                    console.log('---=== interoperability ===---');
-                    console.log(image.interoperability);
-                   
-                    console.log('---=== makernotes ===---');
-                    console.log(image.makernote);
-                }
+            console.log('---=== interoperability ===---');
+            console.log(image.interoperability);
+           
+            console.log('---=== makernotes ===---');
+            console.log(image.makernote);
         }
-    });
+    }
+});
